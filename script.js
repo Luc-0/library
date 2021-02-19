ae2f2a37e2d63ba26d463c2235ffae559e0a1b65
Luc-0 <luc0x00@gmail.com>
2021-02-19 11:42:31 -0300

    Fix firebase db

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const bookList = document.getElementById("book-list");
-const form = document.getElementById("book-form");
-const newButton = document.getElementById("new-btn");
+const bookList = document.getElementById('book-list');
+const form = document.getElementById('book-form');
+const newButton = document.getElementById('new-btn');
 const userPicElement = document.getElementById('user-pic');
 const userNameElement = document.getElementById('user-name');
 const signInButtonElement = document.getElementById('sign-in');
@@ -9,8 +9,8 @@ const mustSignInMessageElement = document.getElementById('must-signin-message');
 
 signOutButtonElement.addEventListener('click', signOut);
 signInButtonElement.addEventListener('click', signIn);
-newButton.addEventListener("click", showForm);
-form.addEventListener("submit", addBookToLibrary);
+newButton.addEventListener('click', showForm);
+form.addEventListener('submit', addBookToLibrary);
 
 const db = firebase.firestore();
 initFirebaseAuth();
@@ -41,7 +41,10 @@ function initFirebaseAuth() {
 }
 
 function getProfilePicUrl() {
-    return firebase.auth().currentUser.photoURL || './images/profile_placeholder.png';
+    return (
+        firebase.auth().currentUser.photoURL ||
+        './images/profile_placeholder.png'
+    );
 }
 
 function getUserName() {
@@ -53,35 +56,38 @@ function isUserSignedIn() {
 }
 
 function authStateObserver(user) {
-    if (user) { // User is signed in!
-      // Get the signed-in user's profile pic and name.
-      var profilePicUrl = getProfilePicUrl();
-      var userName = getUserName();
-  
-      // Set the user's profile pic and name.
-      userPicElement.style.backgroundImage = 'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
-      userNameElement.textContent = userName;
-  
-      // Show user's profile and sign-out button.
-      userNameElement.removeAttribute('hidden');
-      userPicElement.removeAttribute('hidden');
-      signOutButtonElement.removeAttribute('hidden');
-  
-      // Hide sign-in button.
-      signInButtonElement.setAttribute('hidden', 'true');
-    
-      loadDatabaseUserBooks();
-    } else { // User is signed out!
-      // Hide user's profile and sign-out button.
-      userNameElement.setAttribute('hidden', 'true');
-      userPicElement.setAttribute('hidden', 'true');
-      signOutButtonElement.setAttribute('hidden', 'true');
-  
-      // Show sign-in button.
-      signInButtonElement.removeAttribute('hidden');
-      clearForm();
-      hideForm();
-      clearBookList();
+    if (user) {
+        // User is signed in!
+        // Get the signed-in user's profile pic and name.
+        var profilePicUrl = getProfilePicUrl();
+        var userName = getUserName();
+
+        // Set the user's profile pic and name.
+        userPicElement.style.backgroundImage =
+            'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+        userNameElement.textContent = userName;
+
+        // Show user's profile and sign-out button.
+        userNameElement.removeAttribute('hidden');
+        userPicElement.removeAttribute('hidden');
+        signOutButtonElement.removeAttribute('hidden');
+
+        // Hide sign-in button.
+        signInButtonElement.setAttribute('hidden', 'true');
+
+        loadDatabaseUserBooks();
+    } else {
+        // User is signed out!
+        // Hide user's profile and sign-out button.
+        userNameElement.setAttribute('hidden', 'true');
+        userPicElement.setAttribute('hidden', 'true');
+        signOutButtonElement.setAttribute('hidden', 'true');
+
+        // Show sign-in button.
+        signInButtonElement.removeAttribute('hidden');
+        clearForm();
+        hideForm();
+        clearBookList();
     }
 }
 
@@ -99,11 +105,14 @@ function checkSignInWithMessage() {
 
 // Adds a size to Google Profile pics URLs.
 function addSizeToGoogleProfilePic(url) {
-    if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
-      return url + '?sz=150';
+    if (
+        url.indexOf('googleusercontent.com') !== -1 &&
+        url.indexOf('?') === -1
+    ) {
+        return url + '?sz=150';
     }
     return url;
-  }
+}
 
 function loadDatabaseUserBooks() {
     const currentUserId = firebase.auth().currentUser.uid;
@@ -119,7 +128,7 @@ function loadDatabaseUserBooks() {
     });
 }
 
-// 
+//
 function updateDatabaseUserBooks() {
     if (!isUserSignedIn) {
         console.log('User not logged in to update database');
@@ -127,11 +136,14 @@ function updateDatabaseUserBooks() {
     }
     const currentUserId = firebase.auth().currentUser.uid;
     console.log(currentUserId);
-    const userDoc = db.collection('users').doc(currentUserId)
-        .set({books: JSON.stringify(books)}, {merge: true})
+    const userDoc = db
+        .collection('users')
+        .doc(currentUserId)
+        .set({ books: JSON.stringify(books) }, { merge: true })
         .then(() => {
             console.log('Document successfully writen.');
-        }).catch(error => {
+        })
+        .catch((error) => {
             console.error('Error writing document', error);
         });
 }
@@ -148,7 +160,7 @@ function clearBookList() {
 }
 
 function render() {
-    books.forEach(book => {
+    books.forEach((book) => {
         displayBook(book);
     });
 }
@@ -157,24 +169,24 @@ function showForm() {
     if (!checkSignInWithMessage()) {
         return;
     }
-    newButton.style.display = "none";
-    form.style.display = "flex";
+    newButton.style.display = 'none';
+    form.style.display = 'flex';
 }
 
 function hideForm() {
-    newButton.style.display = "inline-block";
-    form.style.display = "none";
+    newButton.style.display = 'inline-block';
+    form.style.display = 'none';
 }
 
 function clearForm() {
     let length = form.elements.length;
     for (let i = 0; i < length; i++) {
         let element = form.elements[i];
-        if (element.type === "checkbox") {
+        if (element.type === 'checkbox') {
             form.elements[i].checked = false;
             console.log(form.elements[i].checked);
         } else {
-            form.elements[i].value = "";
+            form.elements[i].value = '';
         }
     }
 }
@@ -189,7 +201,7 @@ function addBookToLibrary(e) {
     let status = inputs[3].checked;
 
     let newBook = new Book(title, author, pages, status);
-    
+
     books.push(newBook);
     updateDatabaseUserBooks();
     displayBook(newBook);
@@ -197,19 +209,19 @@ function addBookToLibrary(e) {
     hideForm();
 }
 
-function displayBook(book) {    
-    let bookItem = document.createElement("li");
-    bookItem.className = "book-item";
-    
+function displayBook(book) {
+    let bookItem = document.createElement('li');
+    bookItem.className = 'book-item';
+
     // Get array of book values
     let bookValues = Object.values(book);
-    
+
     // Put each book value into a span element
     let length = bookValues.length - 1;
     let spanArr = createSpanArr(bookValues, length);
 
     // Add span to li
-    spanArr.forEach(span => {
+    spanArr.forEach((span) => {
         bookItem.appendChild(span);
     });
 
@@ -223,8 +235,8 @@ function displayBook(book) {
     bookItem.appendChild(checkSpan);
 
     let removeButton = document.createElement('button');
-    removeButton.className = "remove-btn";
-    removeButton.innerText = "X";
+    removeButton.className = 'remove-btn';
+    removeButton.innerText = 'X';
     removeButton.addEventListener('click', removeFromList);
 
     bookItem.appendChild(removeButton);
@@ -267,12 +279,12 @@ function createSpanArr(arr, length) {
 }
 
 function createCheckbox(checked) {
-    if (!(typeof checked === "boolean")){
+    if (!(typeof checked === 'boolean')) {
         return -1;
     }
 
     let checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
+    checkbox.type = 'checkbox';
 
     if (checked) {
         checkbox.checked = true;
